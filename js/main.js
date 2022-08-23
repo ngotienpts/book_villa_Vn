16969b128d8a5734772be9a8cc58956bdf8e553f
NGOVANTIEN <ngotienpts@gmail.com>
2022-08-23 13:59:19 +0700

    23/8/2022

--- js/main.js
+++ js/main.js
@@ -6,22 +6,22 @@ document.addEventListener("DOMContentLoaded", function () {
   var widthDoc = document.querySelector("body");
 
   // header
-  var header = document.querySelector('#header');
+  var header = document.querySelector("#header");
 
   // main
-  var main = document.querySelector('#main');
+  var main = document.querySelector("#main");
 
   // submenu
-  var subMenu = document.querySelector('.sub-menu-wrapper');
+  var subMenu = document.querySelector(".sub-menu-wrapper");
 
-  // navbar arrow 
-  var arrow = document.querySelector('.navbar-arrow');
+  // navbar arrow
+  var arrow = document.querySelector(".navbar-arrow");
 
   // height fixed
-  var heightFixed = document.querySelector('.height-fixed');
+  var heightFixed = document.querySelector(".height-fixed");
 
-  // đếm số lượng 
-  var resortCounts = document.querySelectorAll('.resortCount');
+  // đếm số lượng
+  var resortCounts = document.querySelectorAll(".resortCount");
 
   // show popup login
   var popupLogin = document.querySelector(".popup-login");
@@ -29,8 +29,8 @@ document.addEventListener("DOMContentLoaded", function () {
   var closePopupLogin = document.querySelector(".close-popup-login");
 
   // show search
-  var search = document.querySelector('.search-PC');
-  var openSearch = document.querySelector('.js_show_search');
+  var search = document.querySelector(".search-PC");
+  var openSearch = document.querySelector(".js_show_search");
 
   const app = {
     // su ly cac su kien
@@ -45,45 +45,43 @@ document.addEventListener("DOMContentLoaded", function () {
         };
       }
 
-      // 
-      if(header){
-        var extendsMenu = header.querySelectorAll('.header-extend-icon');
-        extendsMenu.forEach(function(a){
-          if(subMenu){
-            subMenu.style.top = header.clientHeight + 'px';
-            a.onclick = function(){
-              subMenu.classList.toggle('open');
-              if(widthDoc.clientWidth <= 980){
-                widthDoc.classList.toggle('hide');
+      //
+      if (header) {
+        var extendsMenu = header.querySelectorAll(".header-extend-icon");
+        extendsMenu.forEach(function (a) {
+          if (subMenu) {
+            subMenu.style.top = header.clientHeight + "px";
+            a.onclick = function () {
+              subMenu.classList.toggle("open");
+              if (widthDoc.clientWidth <= 980) {
+                widthDoc.classList.toggle("hide");
               }
-            }
+            };
           }
         });
-        
-        if(header){
-          main.style.paddingTop = header.clientHeight + 'px';
-        }
-        if(heightFixed){
-          heightFixed.style.height = header.clientHeight + 'px';
-        }
 
+        if (header) {
+          main.style.paddingTop = header.clientHeight + "px";
+        }
+        if (heightFixed) {
+          heightFixed.style.height = header.clientHeight + "px";
+        }
       }
-
 
       // arrow
-      if(arrow){
-        var navbarList = document.querySelector('.navbar-mb__list');
-        arrow.onclick = function(){
+      if (arrow) {
+        var navbarList = document.querySelector(".navbar-mb__list");
+        arrow.onclick = function () {
           navbarList.scrollLeft += 100;
-        }
+        };
       }
-      
-      if(widthDoc){
-        var expNoEditFull = document.querySelectorAll('.expNoEdit.full');
-        expNoEditFull.forEach(function(a){
-          a.style.width = widthDoc.clientWidth + 'px';
-          a.style.marginLeft = '-' + a.offsetLeft + 'px';
-        })
+
+      if (widthDoc) {
+        var expNoEditFull = document.querySelectorAll(".expNoEdit.full");
+        expNoEditFull.forEach(function (a) {
+          a.style.width = widthDoc.clientWidth + "px";
+          a.style.marginLeft = "-" + a.offsetLeft + "px";
+        });
       }
 
       // show popup login
@@ -101,17 +99,17 @@ document.addEventListener("DOMContentLoaded", function () {
       }
 
       // show search
-      if(openSearch){
-        if(search){
-          openSearch.onclick = function(){
-            if(search.classList.contains('open')){
-              search.classList.remove('open');
-              search.querySelector('.search-PC-input').value = '';
-            }else {
-              search.classList.add('open');
-              search.querySelector('.search-PC-input').focus();
+      if (openSearch) {
+        if (search) {
+          openSearch.onclick = function () {
+            if (search.classList.contains("open")) {
+              search.classList.remove("open");
+              search.querySelector(".search-PC-input").value = "";
+            } else {
+              search.classList.add("open");
+              search.querySelector(".search-PC-input").focus();
             }
-          }
+          };
         }
       }
       // hide cac element khi click ra ngoai
@@ -133,69 +131,81 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     },
     // slider căn được đặt nhiều nhất
-    sliderMostInterestedApartmanet: function(){
+    sliderMostInterestedApartmanet: function () {
       var swiper1 = new Swiper(".mySwiperPartment", {
         slidesPerView: 1,
         slidesPerGroup: 1,
         spaceBetween: 15,
+        grid: {
+          rows: 3,
+          fill: "rows",
+        },
         navigation: {
           nextEl: ".swiper-button-next1",
           prevEl: ".swiper-button-prev1",
         },
-        hideOnClick:true,
+        hideOnClick: true,
         breakpoints: {
-          768 : {
+          768: {
             slidesPerView: 2,
-          slidesPerGroup: 1,
-          spaceBetween: 8,
+            slidesPerGroup: 1,
+            spaceBetween: 8,
+            grid: {
+              rows: 1,
+              fill: "rows",
+            },
           },
           1024: {
             slidesPerView: 3,
             spaceBetween: 10,
             slidesPerGroup: 1,
+            grid: {
+              rows: 1,
+              fill: "rows",
+            },
           },
         },
       });
     },
-     // slider biệt thự nghỉ dưỡng
-     sliderResort: function(){
-      resortCounts.forEach(function(resortCount){
-        var count = Math.floor(resortCount.children.length /2);
+    // slider biệt thự nghỉ dưỡng
+    sliderResort: function () {
+      resortCounts.forEach(function (resortCount) {
+        var count = Math.floor(resortCount.children.length / 2);
         var swiper3 = new Swiper(".mySwiperResort", {
           slidesPerView: 1,
           spaceBetween: 15,
           grid: {
             rows: 3,
-            fill: 'rows',
+            fill: "rows",
           },
           navigation: {
             nextEl: ".swiper-button-next3",
             prevEl: ".swiper-button-prev3",
           },
-          hideOnClick:true,
+          hideOnClick: true,
           breakpoints: {
             768: {
               slidesPerView: 2,
               spaceBetween: 10,
               grid: {
-                rows: (Math.floor(resortCount.children.length /count)),
-                fill: 'rows',
+                rows: Math.floor(resortCount.children.length / count),
+                fill: "rows",
               },
             },
             1024: {
               slidesPerView: 3,
               spaceBetween: 10,
               grid: {
-                rows: (Math.floor(resortCount.children.length /count)),
-                fill: 'rows',
+                rows: Math.floor(resortCount.children.length / count),
+                fill: "rows",
               },
             },
           },
         });
-      })
+      });
     },
     // slider blogs du lich
-    sliderBlogs: function(){
+    sliderBlogs: function () {
       var swiper5 = new Swiper(".mySwiperBlogs", {
         slidesPerView: 1,
         slidesPerGroup: 1,
@@ -203,7 +213,7 @@ document.addEventListener("DOMContentLoaded", function () {
           nextEl: ".swiper-button-next4",
           prevEl: ".swiper-button-prev4",
         },
-        hideOnClick:true,
+        hideOnClick: true,
         breakpoints: {
           1024: {
             slidesPerView: 1,
@@ -213,7 +223,7 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     },
     // slider thư viện ảnh & video
-    sliderPicture: function(){
+    sliderPicture: function () {
       var swiper4 = new Swiper(".mySwiperPiture", {
         slidesPerView: 2,
         slidesPerGroup: 1,
@@ -226,7 +236,7 @@ document.addEventListener("DOMContentLoaded", function () {
           el: ".swiper-pagination2",
           clickable: true,
         },
-        hideOnClick:true,
+        hideOnClick: true,
         breakpoints: {
           1024: {
             slidesPerView: 3,
@@ -236,17 +246,16 @@ document.addEventListener("DOMContentLoaded", function () {
         },
       });
     },
-    flatpickrForm : function () {
-      $(".datepicker").flatpickr(
-          {
-              wrap: true,
-              /*enableTime: true,*/
-              //altInput: true,
-              dateFormat: "d-m-Y",/*Y-m-d H: i*/
-              allowInput: true,
-              minDate: "today"
-          });
-  },
+    flatpickrForm: function () {
+      $(".datepicker").flatpickr({
+        wrap: true,
+        /*enableTime: true,*/
+        //altInput: true,
+        dateFormat: "d-m-Y" /*Y-m-d H: i*/,
+        allowInput: true,
+        minDate: "today",
+      });
+    },
     // window scroll
     windowScroll: function () {
       var _this = this;
